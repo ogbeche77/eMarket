06093a4dd177569590fedc624599efb8e12ade12
ogbeche77 <ogbeche77@yahoo.com>
2020-03-18 17:52:34 +0100

    react fragment

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
   render() {
   return (
-    <div className= "container">
-    <div className = "row">
-    <div className = "col-6">column num one</div>
-    <div className = "col-6">
-    <span>
-    <i className ="fas fa-home" />
-    </span>
-    </div>
-    </div>
-    </div>
+   <React.Fragment> 
+   <h3>this is app.js</h3>
+   </React.Fragment>
   );
 }
 }
