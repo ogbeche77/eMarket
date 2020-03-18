76bf264537680e9f7b006946f3b1a6813b185366
ogbeche77 <ogbeche77@yahoo.com>
2020-03-18 17:59:46 +0100

    initial

---
+++ src/components/Details.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Details extends Component {
+    render() {
+        return (
+            <div>
+               <h3>Here is details</h3> 
+            </div>
+        )
+    }
+}
