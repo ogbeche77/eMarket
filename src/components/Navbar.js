aa97aa4d90c1807679e4399f41895f9f6684a775
ogbeche77 <ogbeche77@yahoo.com>
2020-03-18 17:52:49 +0100

    initial

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>This is nav bar</h3>
+            </div>
+        )
+    }
+}
