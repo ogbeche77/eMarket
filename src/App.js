f2d383f5dec935f74a31cfdf88c7940036ec34af
ogbeche77 <ogbeche77@yahoo.com>
2020-03-18 18:06:29 +0100

    components added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details  from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 
 class App extends Component {
   render() {
   return (
    <React.Fragment> 
-   <h3>this is app.js</h3>
+   <Navbar></Navbar>
+   <ProductList></ProductList>
+   <Details></Details>
+   <Cart></Cart>
+   <Default></Default>
    </React.Fragment>
   );
 }
