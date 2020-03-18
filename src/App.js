6b72e90d648c724ceddccf312b08a2759450cf68
ogbeche77 <ogbeche77@yahoo.com>
2020-03-18 18:29:10 +0100

    route defined

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,11 +14,14 @@ class App extends Component {
   render() {
   return (
    <React.Fragment> 
-   <Navbar></Navbar>
-   <ProductList></ProductList>
-   <Details></Details>
-   <Cart></Cart>
-   <Default></Default>
+   <Navbar/>
+   <Switch>
+   <Route exact path ="/" component={ProductList}/>
+   <Route path ="/details" component={Details}/>
+   <Route path ="/cart" component={Cart}/>
+   <Route  component={Default}/>
+   </Switch>
+   
    </React.Fragment>
   );
 }
