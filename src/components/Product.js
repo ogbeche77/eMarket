d27b67fc35d70173f9f36261701a05dd32f31110
ogbeche77 <ogbeche77@yahoo.com>
2020-03-18 17:59:54 +0100

    initial

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Product extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Here is product</h3>
+            </div>
+        )
+    }
+}
