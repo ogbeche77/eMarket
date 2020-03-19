871a4b04a5905d8102bb9bf414ba528a6a336a75
ogbeche77 <ogbeche77@yahoo.com>
2020-03-19 13:40:24 +0100

    product setup

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react'
 import Product from './Product'
 
 export default class ProductList extends Component {
+    state={
+        products: []
+    }
     render() {
         return (
-            <div>
-                <Product> </Product>
+            <React.Fragment>
+            <div className="py-5">
+            <div className="container">
+            
+
+
+            <div className="row">
             </div>
+            </div>
+            </div>
+            
+            </React.Fragment>
+               // <Product> </Product>
+            
         )
     }
 }
