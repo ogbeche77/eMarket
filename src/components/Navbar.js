04bcdc291e78b70f4e3a9e79f624562bff496fc8
ogbeche77 <ogbeche77@yahoo.com>
2020-03-19 12:54:59 +0100

    cart button styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,4 +33,16 @@ My Cart
 
 const ButtonContainer = styled.button`
 text-transform: capitalize;
-`
+font-size: 1.4rem;
+background: transparent;
+border: 0.2rem solid orange;
+color: orange;
+border-radius: 0.5rem;
+paddding: 0.2rem 0.5rem;
+cursor:pointer;
+margin:0.2rem 0.5rem 0;
+transition: all 0.5s ease-in-out;
+&:hover{
+    background:white;
+}
+`;
