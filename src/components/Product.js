c61afda9ff28c2d8ae85b64d30bbcac6e4849666
ogbeche77 <ogbeche77@yahoo.com>
2020-03-19 15:29:05 +0100

    img container

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,7 +9,7 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                <div className= "img-container p-5" onClick={console.log("image is clicked")}>
+                <div className= "img-container p-5" onClick={()=> console.log("image is clicked")}>
                 <Link to="details">
                 <img src={img} alt="product" className="card-img"/>
                 </Link>
