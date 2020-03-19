58093a6deb669122480154409577a95cd39f81b9
ogbeche77 <ogbeche77@yahoo.com>
2020-03-19 13:51:20 +0100

    store product added

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import Product from './Product'
-
+import Product from './Product';
+import Title from './Title';
+import {storeProducts} from '../data';
 export default class ProductList extends Component {
     state={
         products: []
@@ -10,11 +11,9 @@ export default class ProductList extends Component {
             <React.Fragment>
             <div className="py-5">
             <div className="container">
-            
+            <Title name="our" title="products"/>
 
-
-            <div className="row">
-            </div>
+            <div className="row"/>
             </div>
             </div>
             
