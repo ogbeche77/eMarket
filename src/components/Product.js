f0bbcdac4ee80ff00759229906792cd6562194d6
ogbeche77 <ogbeche77@yahoo.com>
2020-03-20 17:20:52 +0100

    value added

--- src/components/Product.js
+++ src/components/Product.js
@@ -11,16 +11,18 @@ export default class Product extends Component {
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
                 <ProductConsumer>
-                <div className= "img-container p-5" onClick={()=> console.log("image is clicked")}>
+                {(value)=> (
+                    <div className= "img-container p-5" onClick={()=> value.handleDetail(id) }>
                 <Link to="details">
                 <img src={img} alt="product" className="card-img"/>
                 </Link>
                 <button className="cart-btn" disabled={inCart?true : false} 
-                onClick={()=> {console.log("added to cart")
+                onClick={()=> {value.addToCart(id);
                 }}>
                 {inCart?(<p className="text-capitalize mb-0" disabled>in cart</p>) : (<i className="fas fa-cart-plus"/>)}
                 </button>
                 </div>
+                )}
                 </ProductConsumer>
 
 
