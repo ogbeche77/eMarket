b0166a049d6cba68db5846486958e3442e733ce7
ogbeche77 <ogbeche77@yahoo.com>
2020-03-20 11:20:10 +0100

    details updated

--- src/data.js
+++ src/data.js
@@ -148,10 +148,10 @@ export const storeProducts = [
 
 export const detailProduct = {
   id: 1,
-  title: "Google Pixel - Black",
+  title: "Mercedes Benz AMG",
   img:"img/product-16.jpeg",
   price: 10,
-  company: "google",
+  company: "Daimler",
   info:
     "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
   inCart: false,
