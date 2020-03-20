69cf888dc0668257a2f1abd45889e337dc0d7425
ogbeche77 <ogbeche77@yahoo.com>
2020-03-20 18:14:06 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ ReactDOM.render(
     <App />
     </Router>
     </ProductProvider>, 
-    document.getElementById('root'));
+    document.getElementById('root')
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
