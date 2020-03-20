ef82879f3ed5e7508383a80c2bef645cdebd5f70
ogbeche77 <ogbeche77@yahoo.com>
2020-03-20 11:15:44 +0100

    details img

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {ProductConsumer} from '../context';
+import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button';
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-               <h3>Here is details</h3> 
-            </div>
+           <ProductConsumer>
+           {(value)=>{
+           const {id,company, img,info,price, title,inCart} = value.detailProduct;
+           return (
+               <div className="container py-5">
+               {/* title*/}
+               <div className="row">
+               <div className ="col-10 mx-auto text-center text-slanted text-blue my-5">
+                <h1>{title}</h1>
+               </div>
+               </div>
+                {/*end of title*/}
+                {/*product info*/}
+                <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3 ">
+                <img src={img} className="img-fluid" alt="product"/>
+                </div>
+                {/*product text*/}
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                
+                </div>
+                </div>
+                </div>
+           )
+
+           }}
+           </ProductConsumer>
         )
     }
 }
