3f32d979a853a81c43ebfd2a4cd3d89afab76608
ogbeche77 <ogbeche77@yahoo.com>
2020-03-21 14:32:55 +0100

    all imports

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import {ProductConsumer} from '../context';
+import {ButtonContainer} from './Button';
+import {Link} from 'recat-router-dom';
 
 export default class Modal extends Component {
     render() {
         return (
-            <div>
-             <h1>hi modal</h1>   
-            </div>
-        )
+           <ProductConsumer>
+           
+           </ProductConsumer>
+        );
     }
 }
