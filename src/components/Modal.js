fe0f3f45c3947562c809e4d15bc0c8afbcef8dd6
ogbeche77 <ogbeche77@yahoo.com>
2020-03-21 14:29:34 +0100

    modal initial

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Modal extends Component {
+    render() {
+        return (
+            <div>
+             <h1>hi modal</h1>   
+            </div>
+        )
+    }
+}
