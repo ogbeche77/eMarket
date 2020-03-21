26218ec8ebde6df80120a2bd6b011a416cfcc8eb
ogbeche77 <ogbeche77@yahoo.com>
2020-03-21 14:23:55 +0100

    modal func

--- src/components/Product.js
+++ src/components/Product.js
@@ -16,10 +16,11 @@ export default class Product extends Component {
                 <Link to="/details">
                 <img src={img} alt="product" className="card-img-top"/>
                 </Link>
-                <button className="cart-btn" disabled={inCart?true : false} 
+                <button className="cart-btn" disabled={inCart ? true : false} 
                 onClick={()=> {value.addToCart(id);
+                    value.openModal(id);
                 }}>
-                {inCart?(<p className="text-capitalize mb-0" disabled> {" "}in cart</p>) :
+                {inCart ? (<p className="text-capitalize mb-0" disabled> {" "}in cart</p>) :
                  (<i className="fas fa-cart-plus"/>
                  )}
                 </button>
