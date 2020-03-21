76662c7c1895feea25cb6b4e18f21010a688cf28
ogbeche77 <ogbeche77@yahoo.com>
2020-03-21 16:15:12 +0100

    car infos

--- src/data.js
+++ src/data.js
@@ -19,7 +19,7 @@ export const storeProducts = [
     price: 4000,
     company: "Daihatsu",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+     "Daihatsu is an one-off concept car that was shown at the 26th Gaikindo Indonesia International Auto Show in August 2018. It is based on the 1.2 L version of the standard Ayla. The two-tone red and black exterior colour is partially inspired from the 1984 Charade de Tomaso Turbo. The front and rear bumper design has also been redesigned. The 1.2 L 3NR-VE engine has been extensively modified by fitting a turbocharger that is equipped with intercooler, wastegate and blow-off valve with 0.5 bar (7.3 psi) of boost that uprates the power output to 147 kW (197 hp; 200 PS) and torque to 242 N⋅m (178 lbf⋅ft). Further engine changes include the use of low-compression piston, dual fuel injector, open air filter and stronger connecting rod. The 3-module piggyback has been used to manipulate the engine data for performance orientation, while the exhaust system uses a stainless steel free-flow type with 63.5 mm (2.5 in) of diameter. The interior has been stripped down in order to gain lower weight and the roll cage has been added, along with FIA-certified Sparco racing bucket seats. The AC and other unnecessary features has also been removed",
     inCart: false,
     count: 0,
     total: 0
@@ -31,7 +31,7 @@ export const storeProducts = [
     price: 82000,
     company: "General Motors",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Ford Motor Company sells a broad range of automobiles under the Ford marque worldwide, and an additional range of luxury automobiles under the Lincoln marque in the United States. The company has sold vehicles under a number of other marques during its history. The Mercury brand was introduced by Ford in 1939, continuing in production until 2011 when poor sales led to its discontinuation.[123] In 1958, Ford introduced the Edsel brand, but poor sales led to its discontinuation in 1960. In 1985, the Merkur brand was introduced in the United States to market products produced by Ford of Europe; it was discontinued in 1989.",
     inCart: false,
     count: 0,
     total: 0
@@ -43,7 +43,7 @@ export const storeProducts = [
     price: 63000,
     company: "Audi",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The new merged company was incorporated on 1 January 1969 and was known as Audi NSU Auto Union AG, with its headquarters at NSU's Neckarsulm plant, and saw the emergence of Audi as a separate brand for the first time since the pre-war era. Volkswagen introduced the Audi brand to the United States for the 1970 model year. That same year, the mid-sized car that NSU had been working on, the K70, originally intended to slot between the rear-engined Prinz models and the futuristic NSU Ro 80, was instead launched as a Volkswagen.",
     inCart: false,
     count: 0,
     total: 0
@@ -55,7 +55,7 @@ export const storeProducts = [
     price: 41000,
     company: "Daimler",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The Mercedes-AMG GT (C190 / R190) is a sports car produced in coupé and roadster bodystyles by German automobile manufacturer Mercedes-AMG. The car was introduced on 9 September 2014 and was officially unveiled to the public in October 2014 at the Paris Motor Show.[4] After the SLS AMG, it is the second sports car developed entirely in-house by Mercedes-AMG. Formula One driver Lewis Hamilton assisted with the development. The Mercedes-AMG GT went on sale in two variants (GT and GT S) in March 2015, while a GT3 racing variant of the car was introduced in 2015. A high performance variant called the GT R was introduced in 2016. A GT4 racing variant, targeted at semi-professional drivers and based on the GT R variant, was introduced in 2017. All variants are assembled at the Mercedes-Benz plant in Sindelfingen, Germany.",
     inCart: false,
     count: 0,
     total: 0
@@ -67,7 +67,7 @@ export const storeProducts = [
     price: 170000,
     company: "VW",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The Aston Martin DB10 is a bespoke sports car specially developed for the James Bond film Spectre by the British luxury car manufacturer Aston Martin.The car was unveiled by Sam Mendes and Barbara Broccoli, the director and producer of Spectre respectively, the 24th James Bond film produced by Eon Productions. The unveiling took place as part of the official press launch of the film on the 007 Stage at Pinewood Studios, near London, on 4 December 2014.[2] Mendes introduced the car as.The film featured the Aston Martin DB10 as James Bond car.",
     inCart: false,
     count: 0,
     total: 0
@@ -79,7 +79,7 @@ export const storeProducts = [
     price: 33000,
     company: "Jeep",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "The Jeep Grand Cherokee (ZJ) is the first generation of the Jeep Grand Cherokee sport utility vehicle. Introduced in 1992 for the 1993 model year, development of the ZJ Grand Cherokee started under American Motors Corporation (AMC) and was continued after the company was acquired by Chrysler in 1987.Export models produced at the plant in Graz, Austria, were given the vehicle designation of ZG.",
     inCart: false,
     count: 0,
     total: 0
@@ -91,7 +91,7 @@ export const storeProducts = [
     price: 120000,
     company: "Rolls-Royce",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Rolls-Royce Motors Limited was incorporated on 25 April 1971, two and a half months after Rolls-Royce fell into receivership. Under the ownership of the receiver it began to trade in April 1971 manufacturing motor cars, diesel and petrol engines, coachwork and other items previously made by Rolls-Royce's motor car and diesel divisions and Mulliner Park Ward. It continued to take precision engineering work on sub-contracts. In June 1971 it acquired all the business and assets used by the motor car and diesel divisions of Rolls-Royce and Mulliner Park Ward. Rolls-Royce Motors' permitted uses of the various Rolls-Royce trade marks was very precisely defined.[2]At the end of 1972 Rolls-Royce Motors employees in the United Kingdom were 5,855 in the car division and 2,311 in the Diesel division, a total of 8,166 people.",
     inCart: false,
     count: 0,
     total: 0
@@ -103,7 +103,7 @@ export const storeProducts = [
     price: 120000,
     company: "Lamborghini",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      " Automobili Ferruccio Lamborghini S.p.A. in 1963 to compete with established marques, including Ferrari. The company was noted for using a rear mid-engine, rear-wheel drive. Lamborghini grew rapidly during its first decade, but sales plunged in the wake of the 1973 worldwide financial downturn and the oil crisis. The firm's ownership changed three times after 1973, including a bankruptcy in 1978. American Chrysler Corporation took control of Lamborghini in 1987 and sold it to Malaysian investment group Mycom Setdco and Indonesian group V'Power Corporation in 1994",
     inCart: false,
     count: 0,
     total: 0
@@ -115,7 +115,7 @@ export const storeProducts = [
     price: 72000,
     company: "Range Roger",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Land Rover is a British brand of four-wheel drive cars, that exclusively offers premium and luxury sport utility vehicles. Owned by multinational car manufacturer Jaguar Land Rover (JLR), which has been owned by India's Tata Motors since 2008,[4] JLR currently build Land Rovers in Brazil, China, India, Slovakia, and the United Kingdom.",
     inCart: false,
     count: 0,
     total: 0
@@ -127,7 +127,7 @@ export const storeProducts = [
     price: 91000,
     company: "Audi",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Audi AI is a driver assist feature offered by Audi. The company's stated intent is to offer fully autonomous driving at a future time, acknowledging that legal, regulatory and technical hurdles must be overcome to achieve this goal. On June 4, 2017, Audi stated that its new A8 will be fully self-driving for speeds up to 60 km/h using its Audi AI. Contrary to other cars, the driver will not have to do safety checks such as touching the steering wheel every 15 seconds to use this feature. The Audi A8 will therefore be the first production car to reach level 3 autonomous driving, meaning that the driver can safely turn their attention away from driving tasks",
     inCart: false,
     count: 0,
     total: 0
@@ -139,7 +139,7 @@ export const storeProducts = [
     price: 172000,
     company: "Ferarri",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Enzo Ferrari oversaw the launch of the Ferrari F40, the last new Ferrari launched before his death later that year. In 1989, the company was renamed Ferrari S.p.A.[15] From 2002 to 2004, Ferrari produced the Enzo, their fastest model at the time, which was introduced and named in honor of the company's founder, Enzo Ferrari. It was to be called the F60, continuing on from the F40 and F50, but Ferrari was so pleased with it, they called it the Enzo instead. It was initially offered to loyal and recurring customers, each of the 399 made (minus the 400th which was donated to the Vatican for charity) had a price tag of $650,000 apiece (equivalent to £400,900).",
     inCart: false,
     count: 0,
     total: 0
