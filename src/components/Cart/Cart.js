e941c190b0c7c46b9d13bf7cc05ec005db0be654
ogbeche77 <ogbeche77@yahoo.com>
2020-03-22 12:52:35 +0100

    cart title

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Title from '../Title';
 
 export default class Cart extends Component {
     render() {
         return (
-            <div>
-               <h3>Hello from cart</h3> 
-            </div>
+            <section>
+               <Title name="your" title="cart"/>
+               </section>
         )
     }
 }
