c537348578f2f5dc408348237b289a1b2d7d4410
ogbeche77 <ogbeche77@yahoo.com>
2020-03-22 13:19:14 +0100

    empty cart initial

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Title from '../Title';
-import CartColumns from './CartColums';
+import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
 
 export default class Cart extends Component {
     render() {
@@ -8,6 +9,7 @@ export default class Cart extends Component {
             <section>
                <Title name="your" title="cart"/>
                <CartColumns/>
+               <EmptyCart/>
                </section>
         )
     }
