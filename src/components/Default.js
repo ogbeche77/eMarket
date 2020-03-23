eea83f1946a377d958e7fca995d680035109a95d
ogbeche77 <ogbeche77@yahoo.com>
2020-03-23 11:48:31 +0100

    resized

--- src/components/Default.js
+++ src/components/Default.js
@@ -6,7 +6,10 @@ export default class Default extends Component {
             <div className="container">
             <div className="row">
             <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
-            <h1 className="dispaly-3"> 404 </h1>
+            <h1 className="display-1"> 404 </h1>
+            <h1> ERROR</h1>
+            <h2>page not found</h2>
+            <h3>Requested URL </h3>
             </div>
             </div>
             </div>
