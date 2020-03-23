0ff5a26df0ae0bb310c6e1383b28782fc97268ab
ogbeche77 <ogbeche77@yahoo.com>
2020-03-23 12:38:53 +0100

    history added

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import PaypalButton from './PaypalButton';
 
-export default function CartTotals({value}) {
+export default function CartTotals({value, history}) {
     const {cartSubTotal, cartTax, cartTotal, clearCart} = value;
 
     return  (
@@ -31,6 +32,9 @@ export default function CartTotals({value}) {
         cartTotal: </span>
         <strong> € {cartTotal} </strong>
         </h5>
+        <PaypalButton total={cartTotal} clearCart={clearCart} history={history}>
+        
+        </PaypalButton>
         </div>
         </div>
         </div>
