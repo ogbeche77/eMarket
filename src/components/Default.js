4cddd851c07b7b18343e6a064d338bac10b83eb2
ogbeche77 <ogbeche77@yahoo.com>
2020-03-23 11:43:44 +0100

    404 page

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react'
 export default class Default extends Component {
     render() {
         return (
-            <div>
-                <h3> Page not found</h3>
+            <div className="container">
+            <div className="row">
+            <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="dispaly-3"> 404 </h1>
+            </div>
+            </div>
             </div>
         )
     }
