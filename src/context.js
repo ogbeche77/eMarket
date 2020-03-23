a5bb8ae2ca8bd10434c3dc61b69db35e81881105
ogbeche77 <ogbeche77@yahoo.com>
2020-03-23 11:33:37 +0100

    decrement funct

--- src/context.js
+++ src/context.js
@@ -77,10 +77,8 @@ closeModal = ()=>{
 increment =(id)=>{
     let tempCart =[...this.state.cart];
     const selectedProduct = tempCart.find(item=>item.id === id);
-
     const index = tempCart.indexOf(selectedProduct);
     const product = tempCart[index];
-
     product.count = product.count + 1;
     product.total = product.count * product.price;
 
@@ -88,7 +86,17 @@ increment =(id)=>{
 
 };
 decrement = (id)=>{
-    console.log("decrement");
+    let tempCart =[...this.state.cart];
+    const selectedProduct = tempCart.find(item=>item.id === id);
+
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count - 1;
+    if(product.count === 0){
+        this.removeItem(id)
+    } else{
+        product
+    }
 };
 removeItem = (id)=>{
     let tempProducts = [...this.state.products];
