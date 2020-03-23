41ef445a5fde312171d87a8855bac45b515a9b64
ogbeche77 <ogbeche77@yahoo.com>
2020-03-23 11:51:35 +0100

    error page

--- src/components/Default.js
+++ src/components/Default.js
@@ -5,11 +5,11 @@ export default class Default extends Component {
         return (
             <div className="container">
             <div className="row">
-            <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <div className="col-10 mx-auto text-primary text-center text-title text-uppercase pt-5">
             <h1 className="display-1"> 404 </h1>
             <h1> ERROR</h1>
             <h2>page not found</h2>
-            <h3>Requested URL </h3>
+            <h3>Requested URL <span className="text-danger">{this.props.location.pathname}</span>{" "} was not found </h3>
             </div>
             </div>
             </div>
