1983c5203b952b82670cfada65e15c4b3905ed32
ogbeche77 <ogbeche77@yahoo.com>
2020-03-26 14:51:48 +0100

    product cart hidden

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,7 +17,8 @@ export default class Product extends Component {
 
                 <img src={img} alt="product" className="card-img-top"/>
                 </Link>
-                <button className="cart-btn" disabled={inCart ? true : false} 
+
+               { /*<button className="cart-btn" disabled={inCart ? true : false} 
                 onClick={()=> {
                     value.addToCart(id);
                     value.openModal(id);
@@ -31,7 +32,7 @@ export default class Product extends Component {
                 ) : (
                     <i className="fas fa-cart-plus"/>
                  )}
-                </button>
+                </button> */}
                 </div>)}
                 </ProductConsumer>
 
