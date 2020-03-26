af29222d534701e30f9fefae5aa75fab89453385
ogbeche77 <ogbeche77@yahoo.com>
2020-03-26 14:52:39 +0100

    in cart rmv

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 
 export default class Product extends Component {
     render() {
-        const{id, title, img, price, inCart } = this.props.product;
+        const{id, title, img, price } = this.props.product;
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
