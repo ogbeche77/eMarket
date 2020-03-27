588a0272dd1557ded8113122525cae765ede2646
ogbeche77 <ogbeche77@yahoo.com>
2020-03-27 11:39:36 +0100

    formated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,43 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-import img from '../logos.svg';
-import styled from 'styled-components';
-import {ButtonContainer} from './Button';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import img from "../logos.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class Navbar extends Component {
-    render() {
-        return (
-           <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-           {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
+  render() {
+    return (
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk  */}
-<Link to= "/">
-<img src={img} alt="store" className="navbar-brand" />
-</Link>
-<ul className="navbar-nav align-items-center">
-<li className="nav-item ml-5">
-<Link to="/" className="nav-link">
-JS Automobiles
-</Link>
-</li>
-</ul>
-<Link to="/cart" className="ml-auto">
-<ButtonContainer>
-<span className="mr-2"><i className="fas fa-cart-plus"/> </span>
-My Cart
-</ButtonContainer>
-</Link>
- </NavWrapper>
-        );
-    }
+        <Link to="/">
+          <img src={img} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              JS Automobiles
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />{" "}
+            </span>
+            My Cart
+          </ButtonContainer>
+        </Link>
+      </NavWrapper>
+    );
+  }
 }
 
 const NavWrapper = styled.nav`
-background: grey;
-.nav-link{
-color:white !important;
-font-size: 1.3rem;
-
-}
+  background: grey;
+  .nav-link {
+    color: white !important;
+    font-size: 1.3rem;
+  }
 `;
-
