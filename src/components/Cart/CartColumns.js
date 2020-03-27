db635b6473758d49182ba38fa3a44b7a610eb3c6
ogbeche77 <ogbeche77@yahoo.com>
2020-03-27 11:37:07 +0100

    formated

--- src/components/Cart/CartColumns.js
+++ src/components/Cart/CartColumns.js
@@ -1,29 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function CartColums() {
-    return (
-        <div className="container-fluid text-center d-none d-lg-block font-weight-bold">
-            <div className="row">
-
-        <div className= "col-10 mx-auto col-lg-2">
-         <p className="text-uppercase">products</p>
+  return (
+    <div className="container-fluid text-center d-none d-lg-block font-weight-bold">
+      <div className="row">
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">products</p>
         </div>
-        <div className= "col-10 mx-auto col-lg-2">
-         <p className="text-uppercase">name of product</p>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">name of product</p>
         </div>
-        <div className= "col-10 mx-auto col-lg-2">
-         <p className="text-uppercase">price</p>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">price</p>
         </div>
-        <div className= "col-10 mx-auto col-lg-2">
-         <p className="text-uppercase">quantity</p>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">quantity</p>
         </div>
-        <div className= "col-10 mx-auto col-lg-2">
-         <p className="text-uppercase">remove</p>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">remove</p>
         </div>
-        <div className= "col-10 mx-auto col-lg-2">
-         <p className="text-uppercase">total</p>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">total</p>
         </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
