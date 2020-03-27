41daecdd83e401276211f8f684c6f9eaf863f323
ogbeche77 <ogbeche77@yahoo.com>
2020-03-27 11:35:23 +0100

    formated

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,44 +1,46 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import PaypalButton from './PaypalButton';
+import React from "react";
+import { Link } from "react-router-dom";
+import PaypalButton from "./PaypalButton";
 
-export default function CartTotals ({value, history}) {
-    const {cartSubTotal, cartTax, cartTotal, clearCart} = value;
+export default function CartTotals({ value, history }) {
+  const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
 
-    return  (
-        <React.Fragment>
-        <div className="container">
+  return (
+    <React.Fragment>
+      <div className="container">
         <div className="row">
-        <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
-        <Link to="/">
-        <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={()=>clearCart()}>
-        clear cart
-        </button>
-        </Link>
-        <h5>
-        <span className="text-title">
-        Sub-total: </span>
-        <strong> € {cartSubTotal} </strong>
-        </h5>
+          <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+            <Link to="/">
+              <button
+                className="btn btn-outline-danger text-uppercase mb-3 px-5"
+                type="button"
+                onClick={() => clearCart()}
+              >
+                clear cart
+              </button>
+            </Link>
+            <h5>
+              <span className="text-title">Sub-total: </span>
+              <strong> € {cartSubTotal} </strong>
+            </h5>
 
-        <h5>
-        <span className="text-title">
-        Cart-tax: </span>
-        <strong> € {cartTax} </strong>
-        </h5>
+            <h5>
+              <span className="text-title">Cart-tax: </span>
+              <strong> € {cartTax} </strong>
+            </h5>
 
-        <h5>
-        <span className="text-title">
-        cart-Total: </span>
-        <strong> € {cartTotal} </strong>
-        </h5>
-        <PaypalButton total={cartTotal} clearCart={clearCart} history={history}>
-        
-        </PaypalButton>
+            <h5>
+              <span className="text-title">cart-Total: </span>
+              <strong> € {cartTotal} </strong>
+            </h5>
+            <PaypalButton
+              total={cartTotal}
+              clearCart={clearCart}
+              history={history}
+            ></PaypalButton>
+          </div>
         </div>
-        </div>
-        </div>
-        </React.Fragment>
-
-    );   
+      </div>
+    </React.Fragment>
+  );
 }
