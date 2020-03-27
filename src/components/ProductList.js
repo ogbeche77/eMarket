4b385dd3985c5cfd8de49dfce19d4e005273d14a
ogbeche77 <ogbeche77@yahoo.com>
2020-03-27 11:36:03 +0100

    formated

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,32 +1,28 @@
-import React, { Component } from 'react'
-import Product from './Product';
-import Title from './Title';
-import {ProductConsumer} from '../context';
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { ProductConsumer } from "../context";
 export default class ProductList extends Component {
-    
-    render() {
-        return (
-            <React.Fragment>
-            <div className="py-5">
-            <div className="container">
-            <Title name="Automobiles " title="In Stock"/>
+  render() {
+    return (
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="Automobiles " title="In Stock" />
 
             <div className="row">
-            <ProductConsumer>
-            {value=>{
-           return value.products.map( product=>{
-            return <Product key = {product.id} product ={product}/>;
-           })
-            }}
-            
-            </ProductConsumer>
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
             </div>
-            </div>
-            </div>
-            
-            </React.Fragment>
-               // <Product> </Product>
-            
-        )
-    }
+          </div>
+        </div>
+      </React.Fragment>
+      // <Product> </Product>
+    );
+  }
 }
