3a10adb9c8481bdc8bdd15a8e84654364a9a119b
ogbeche77 <ogbeche77@yahoo.com>
2020-03-27 11:38:32 +0100

    formated

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,18 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
-text-transform: capitalize;
-font-size: 1.4rem;
-background: transparent;
-border: 0.2rem solid orange;
-border-color: ${props => props.cart? "orange" : "blue"};
-color:${prop => prop.cart? "orange" : "blue"};
-border-radius: 0.5rem;
-paddding: 0.2rem 0.5rem;
-cursor:pointer;
-margin:0.2rem 0.5rem 0;
-transition: all 0.5s ease-in-out;
-&:hover{
-    background:${prop => prop.cart? "grey" : "grey"};
-}
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.2rem solid orange;
+  border-color: ${props => (props.cart ? "orange" : "blue")};
+  color: ${prop => (prop.cart ? "orange" : "blue")};
+  border-radius: 0.5rem;
+  paddding: 0.2rem 0.5rem;
+  cursor: pointer;
+  margin: 0.2rem 0.5rem 0;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    background: ${prop => (prop.cart ? "grey" : "grey")};
+  }
 `;
