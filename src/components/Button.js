7d285e8a9648e6e3fd29386bab8a79eeba5eb79e
ogbeche77 <ogbeche77@yahoo.com>
2020-04-25 15:36:32 +0200

    comments

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
-
+ 
+//props.cart ? ensures color changes when item added to cart
 export const ButtonContainer = styled.button`
   text-transform: capitalize;
   font-size: 1.4rem;
   background: transparent;
   border: 0.2rem solid orange;
-  border-color: ${props => (props.cart ? "orange" : "blue")};
+  border-color: ${props => (props.cart ? "orange" : "blue")}; 
   color: ${prop => (prop.cart ? "orange" : "blue")};
   border-radius: 0.5rem;
   paddding: 0.2rem 0.5rem;
