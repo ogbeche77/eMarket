21c8abfe1ff8e8db3c0ade7a6807bfaf13621832
ogbeche77 <ogbeche77@yahoo.com>
2020-04-25 14:39:29 +0200

    comment

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../context";
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"; //comes with react
 
 export default class Product extends Component {
   render() {
