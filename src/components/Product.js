7cb31c7756263c3ed128d880f9d2d7c582dcfb90
ogbeche77 <ogbeche77@yahoo.com>
2020-04-26 11:11:41 +0200

    comments

--- src/components/Product.js
+++ src/components/Product.js
@@ -14,7 +14,7 @@ export default class Product extends Component {
             {value => (
               <div
                 className="img-container p-5"
-                onClick={() => value.handleDetail(id)}
+                onClick={() => value.handleDetail(id)} //handleDetail method also on context.js
               >
                 <Link to="/details">
                   <img src={img} alt="product" className="card-img-top" />
