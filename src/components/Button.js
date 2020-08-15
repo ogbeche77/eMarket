1c6a45b01ae93342bc9633a03db0a4c706299b79
ogbeche77 <ogbeche77@yahoo.com>
2020-08-15 09:38:42 +0200

    Nav button color and hover changed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
- 
+
 //props.cart ? ensures color changes when item added to cart
 export const ButtonContainer = styled.button`
   text-transform: capitalize;
   font-size: 1.4rem;
   background: transparent;
   border: 0.2rem solid orange;
-  border-color: ${props => (props.cart ? "orange" : "blue")}; 
-  color: ${prop => (prop.cart ? "orange" : "dark")};
+  border-color: ${props => (props.cart ? "blue" : "blue")}; 
+  color: ${prop => (prop.cart ? "blue" : "blue")};
   paddding: 0.2rem 0.5rem;
   cursor: pointer;
   margin: 0.2rem 0.5rem 0;
