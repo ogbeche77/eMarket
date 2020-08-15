27c7bdb3f769c8cf33a89f76dc4ddcefc4d37eaf
ogbeche77 <ogbeche77@yahoo.com>
2020-08-15 09:39:13 +0200

    Comment removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,6 @@ export default class Navbar extends Component {
   render() {
     return (
       <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-        {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
-Creative Commons (Attribution 3.0 Unported);
-https://www.iconfinder.com/Makoto_msk  */}
         <Link to="/">
           <img src={img} alt="store" className="navbar-brand" />
         </Link>
